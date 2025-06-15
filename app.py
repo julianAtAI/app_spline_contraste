b97b67e8470c04904ea943538e19569ecc93c3a6
julianAtAI <zubsci@gmail.com>
2025-06-14 18:14:37 -0600

    Version para render 1.1

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def result_image(filename):
     return send_file(os.path.join(RESULT_FOLDER, filename), mimetype="image/png")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto de Render o 5000 local
+    app.run(host="0.0.0.0", port=port)
